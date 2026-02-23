6064ba62207df71bc960faabc81c296d3b09d4b4
nolanr-dev <nolanribaud@gmail.com>
2026-02-23 15:26:09 +0100

    j'ai bien mocker (mdr)

--- server/tests/getCountriesStartingWith.spec.ts
+++ server/tests/getCountriesStartingWith.spec.ts
@@ -1,7 +1,19 @@
 import { getCountriesStartingWith } from "../src/utils/getCountriesStartingWith";
+import axios from "axios";
+
+jest.mock("axios");
 
 describe("getCountriesStartingWith", () => {
   it("should return an array of countries starting with the search word", async () => {
+      (axios.get as jest.Mock).mockResolvedValue({
+          data: {
+              data: {
+                  FR: {country: "France"},
+                  DE: {country: "Germany"},
+                  IT: {country: "Italy"},
+              },
+          },
+      });
     const countries = await getCountriesStartingWith("France");
     expect(countries).toEqual(["France"]);
   });
