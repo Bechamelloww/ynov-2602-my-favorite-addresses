35ea0161a6a38a43d6a4f6fd7522167e76889997
nolanr-dev <nolanribaud@gmail.com>
2026-02-24 12:11:26 +0100

    test: add user creation test with faker data and initialize datasource

--- server/tests/app.spec.ts
+++ server/tests/app.spec.ts
@@ -1,7 +1,28 @@
+import request from "supertest";
+import { faker } from "@faker-js/faker";
 import app from "../src/app";
+import datasource from "../src/datasource";
 
-describe("app", () => {
-  it("should be defined", () => {
-    expect(app).toBeDefined();
-  });
+beforeAll(async () => {
+    await datasource.initialize();
+});
+
+afterAll(async () => {
+    await datasource.destroy();
+});
+
+describe("POST /api/users", () => {
+    it("should create a user account", async () => {
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+
+        const res = await request(app)
+            .post("/api/users")
+            .send({ email, password })
+            .expect(200);
+
+        expect(res.body.item).toBeDefined();
+        expect(res.body.item.email).toBe(email);
+        expect(res.body.item.id).toBeDefined();
+    });
 });
